c5b2a9a0251f5f9a4048d226c0d2be9d84f21c75
xoni.xxt <jgabrielwrk@gmail.com>
2025-09-13 20:46:04 -0300

    detalhes_pessoa e personagem favorito por id

--- aplicativo/urls.py
+++ aplicativo/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'aplicativo'
 
 urlpatterns = [
-    path('', views.lista_pessoas, name='pessoas_lista')
+    path('', views.lista_pessoas, name='pessoas_lista'),
+    path('<int:pk>/', views.detalhes_pessoa, name='detalhes_pessoa')
+
                ]
--- aplicativo/views.py
+++ aplicativo/views.py
@@ -1,6 +1,7 @@
 from .forms import PessoaForm
 from .models import PessoaModel
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from .api import get_personagem_por_id
 
 
 def lista_pessoas(request):
@@ -15,6 +16,13 @@ def lista_pessoas(request):
     pessoas = PessoaModel.objects.all()
     contexto = {'pessoas': pessoas, 'form': form}
     return render(request, 'aplicativo/pessoas_lista.html', contexto)
+
+def detalhes_pessoa(request, pk):
+    pessoa = get_object_or_404(PessoaModel, pk=pk)
+    personagem = None
+    if pessoa.personagem_favorito:
+        personagem = get_personagem_por_id(pessoa.personagem_favorito)
+    return render(request, 'aplicativo/detalhes_pessoa.html', {'pessoa': pessoa, 'personagem': personagem})
 
 
 
