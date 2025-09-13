a289f84019d53cbe67b7599c561bffc0bb46fcf7
xoni.xxt <jgabrielwrk@gmail.com>
2025-09-13 20:31:30 -0300

    forms prontos, funcao lista e preparando detalhes_pessoa

--- aplicativo/views.py
+++ aplicativo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from .forms import PessoaForm
+from .models import PessoaModel
+from django.shortcuts import render, redirect
+
+
+def lista_pessoas(request):
+    if request.method == 'POST':
+        form = PessoaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('aplicativo:pessoas_lista')
+    else:
+        form = PessoaForm()
+
+    pessoas = PessoaModel.objects.all()
+    contexto = {'pessoas': pessoas, 'form': form}
+    return render(request, 'aplicativo/pessoas_lista.html', contexto)
+
+
 
 # Create your views here.
