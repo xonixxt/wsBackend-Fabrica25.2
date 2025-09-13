731379414db6cf962dfa5fa381415c247de0bde6
xoni.xxt <jgabrielwrk@gmail.com>
2025-09-13 20:14:04 -0300

    criacao do models Pessoa e Personagem

--- aplicativo/models.py
+++ aplicativo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class PersonagemModel(models.Model):
+    nome = models.CharField(max_length=100)
+
+from django.db import models
+
+class PessoaModel(models.Model):
+    nome = models.CharField(max_length=100)
+    personagem_favorito = models.IntegerField(default=0)
